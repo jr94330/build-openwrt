5e9710adce2a395d65d8e962c2ffb9703e46560c
jr94330 <57557508+jr94330@users.noreply.github.com>
2024-06-01 15:32:10 +0800

    Update diy-part2.sh
    2024.6.1增加插件
    luci-app-accesscontrol
    luci-app-bird1-ipv4
    luci-app-bird1-ipv6
    luci-app-arpbind
    luci-app-autoreboot
    luci-app-ddns
    luci-app-ddns-go
    luci-app-filetransfer
    luci-app-firewall
    luci-app-mwan3
    luci-app-nlbwmon
    luci-app-samba
    luci-app-samba4
    luci-app-ssr-plus
    luci-app-syncdial
    luci-app-turboacc
    luci-app-upnp
    luci-app-usb-printer
    luci-app-vlmcsd
    luci-app-vsftpd
    luci-app-wol
    luci-app-clash
    luci-app-ramfree
    luci-app-easymesh
    luci-app-argon-config
    luci-app-attendedsysupgrade
    luci-app-docker
    luci-app-fileassistant
    luci-app-aria2
    luci-app-passwall 
    luci-app-unblockmusic
    luci-app-webadmin
    luci-app-uhttpd
    luci-app-update
    luci-app-minidlna
    ，修改默认IP

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -9,7 +9,7 @@
 # ------------------------------- Main source started -------------------------------
 #
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
-# sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
@@ -19,17 +19,70 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
 #
 # ------------------------------- Main source ends -------------------------------
 
+# ------------------------------- Add software sources -------------------------------
+# Add software sources
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git haibo https://github.com/haiibo/openwrt-packages" >> feeds.conf.default
+echo "src-git liuran001 https://github.com/liuran001/openwrt-packages" >> feeds.conf.default
+# ------------------------------- Add software sources ends -------------------------------
+
+# ------------------------------- Update and install feeds -------------------------------
+# Update and install feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+# ------------------------------- Update and install feeds ends -------------------------------
+
+# ------------------------------- Install luci applications -------------------------------
+# Install luci applications
+./scripts/feeds install luci-app-accesscontrol
+./scripts/feeds install luci-app-bird1-ipv4
+./scripts/feeds install luci-app-bird1-ipv6
+./scripts/feeds install luci-app-arpbind
+./scripts/feeds install luci-app-autoreboot
+./scripts/feeds install luci-app-ddns
+./scripts/feeds install luci-app-ddns-go
+./scripts/feeds install luci-app-filetransfer
+./scripts/feeds install luci-app-firewall
+./scripts/feeds install luci-app-mwan3
+./scripts/feeds install luci-app-nlbwmon
+./scripts/feeds install luci-app-samba
+./scripts/feeds install luci-app-samba4
+./scripts/feeds install luci-app-ssr-plus
+./scripts/feeds install luci-app-syncdial
+./scripts/feeds install luci-app-turboacc
+./scripts/feeds install luci-app-upnp
+./scripts/feeds install luci-app-usb-printer
+./scripts/feeds install luci-app-vlmcsd
+./scripts/feeds install luci-app-vsftpd
+./scripts/feeds install luci-app-wol
+./scripts/feeds install luci-app-clash
+./scripts/feeds install luci-app-ramfree
+./scripts/feeds install luci-app-easymesh
+./scripts/feeds install luci-app-argon-config
+./scripts/feeds install luci-app-attendedsysupgrade
+./scripts/feeds install luci-app-docker
+./scripts/feeds install luci-app-fileassistant
+./scripts/feeds install luci-app-aria2
+./scripts/feeds install luci-app-passwall
+./scripts/feeds install luci-app-unblockmusic
+./scripts/feeds install luci-app-webadmin
+./scripts/feeds install luci-app-uhttpd
+./scripts/feeds install luci-app-update
+./scripts/feeds install luci-app-minidlna
+# ------------------------------- Install luci applications ends -------------------------------
+
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
@@ -50,4 +103,3 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
-
